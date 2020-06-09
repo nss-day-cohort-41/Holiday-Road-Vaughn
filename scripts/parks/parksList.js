82eaeda8a9610de5f8a06bf3d8dba84cf4511922
Rob Mixon <mixonroberth@gmail.com>
2020-06-09 14:46:59 -0500

    commiting to merge with master

--- scripts/parks/parksList.js
+++ scripts/parks/parksList.js
@@ -1,12 +1,28 @@
-/* park code value key to apply with object.parkCode to get fetch call
-in each selector choice */
+
+/* responsible for making a list of park html representations
+and putting them into the browser */
+//import 
+
+
 
 const showParkData = (parkData) => {
-    for (const parkObject of parkData) {
-        const parkHTML
+    //iterate the collection of park objects
+    for (const currentParkObject of parkData) {
+        //convert the current obj to its html
+        const parkHTML = parkConverter(currentParkObject)
+        // find the <article> element in index.html
+        const parkArticleElement = document.querySelector(".park__list")
+        //put the location html representation inside the <article> element
+        parkArticleElement.innerHTML += parkHTML
     }
 }
 
+/* This function will convert a single location object to a HTML representation 
+and return it */
 
-let targetSelector = document.querySelector(".park__list")
-    switch(targetSelector.value)
+const parkConverter = (parkObject) => {
+    const parkHTMLRepresentation = `
+    <select name = "park__list" id="">
+        <option value="park">${parkObject.parkName}
+    `
+}
