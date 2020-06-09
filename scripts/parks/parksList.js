7b133766bf39ed1a908721273cf8322471bab21d
Rob Mixon <mixonroberth@gmail.com>
2020-06-09 13:01:30 -0500

    prelunch foundational work

---
+++ scripts/parks/parksList.js
@@ -0,0 +1,12 @@
+/* park code value key to apply with object.parkCode to get fetch call
+in each selector choice */
+
+const showParkData = (parkData) => {
+    for (const parkObject of parkData) {
+        const parkHTML
+    }
+}
+
+
+let targetSelector = document.querySelector(".park__list")
+    switch(targetSelector.value)
