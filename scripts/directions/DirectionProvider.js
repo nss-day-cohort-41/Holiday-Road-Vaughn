91dc301b326e21d54a77c829914a01d131a8708d
Tasha Lane <designtmg77@gmail.com>
2020-06-11 11:51:46 -0500

    updated

--- scripts/directions/DirectionProvider.js
+++ scripts/directions/DirectionProvider.js
@@ -1,7 +1,7 @@
 let directions = []
 
 const getDirections = (lat, long, apiKey) => {
-    return fetch(`https://graphhopper.com/api/1/geocode?q=national+park&locale=us&point=${lat},${long}&vehicle=car&locale=us&instructions =true&calc_points&debug=true&key=35c189ee-0b68-474a-9be6-79ece436efed`)
+    return fetch(`https://graphhopper.com/api/1/geocode?q=national+park&locale=us&point=${lat},${long}&vehicle=car&locale=us&instructions =true&calc_points&debug=true&key=${apiKey}`)
         .then(response => response.json())
         .then(parseDirections => {
             directions = parseDirections.path.slice()
