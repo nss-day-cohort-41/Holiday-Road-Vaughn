207357e1b3d0812db7c4ab5e1500fae0aaecd271
Rob Mixon <mixonroberth@gmail.com>
2020-06-11 13:38:15 -0500

    attempting to make button work

--- scripts/parks/parkDetailButton.js
+++ scripts/parks/parkDetailButton.js
@@ -1,5 +1,7 @@
 
 
+
+
 let parkDetailsButton = document.querySelector(".show__park_details").addEventListener("click", clickEvent=> {
     let parkDetailsArticle = document.querySelector(".park__selected").classList.toggle("hidden")
     }
