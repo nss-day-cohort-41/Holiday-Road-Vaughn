faecfd6dc30b2c04dfff449f5a966152d447dbdc
Jonrrile <jonrrile@ut.utm.edu>
2020-06-11 13:22:28 -0500

    cleaned up my eateries code

--- scripts/eateries/Eateries.js
+++ scripts/eateries/Eateries.js
@@ -12,58 +12,12 @@ const eateryDetailsConverter = (eateryObject) => {
 
 
 
+  const eateryConverter = (eateryObject) => {
 
+    const eateryHTMLRepresentation = `<select name="attraction__list" id="">
+    <option value=${eateryObject.id}>${eateryObject.businessName}</option>
+</select>`
 
+    return eateryHTMLRepresentation
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const eateryContentTarget = document.querySelector(".eatery__selected")
-const clearEateryData = () => eateryContentTarget.innerHTML = ""
-
-const showEateryData = (eateryData) => {
-    for (const data of eateryData.data) {
-        const eateryHTML = eateryDetailConverter(data)
-        const eateryArticleElement = document.querySelector(".eatery__selected")
-        eateryArticleElement.innerHTML += parkHTML
-    }
-}
-const eateryDetailConverter = (data) => {
-    const eateryHTML = `
-    <section class="eatery">
-        <p> ${eateryData.data[0].businessName} </p>
-    </section>
-    `
-    return eateryHTML;
-}
-*/
+} 
--- scripts/eateries/EateryList.js
+++ scripts/eateries/EateryList.js
@@ -31,10 +31,10 @@ eateryDetailsButton.addEventListener("click", (event) => {
                     
                 }
             }
-            // This data is Taco
+            
             return iterator
         }).then((data) => { 
-            goku(data)
+            populateDropdown(data)
             
             
         })
@@ -42,17 +42,6 @@ eateryDetailsButton.addEventListener("click", (event) => {
 
     getEateryDetails()
 });
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 Responsible for generating a list of eatery HTML
@@ -74,20 +63,7 @@ const eateryList = () => {
 }
 
 
-/*
-    This function will convert a single location object to an
-    HTML representation and return it
-*/
-const eateryConverter = (eateryObject) => {
-
-    const eateryHTMLRepresentation = `<select name="attraction__list" id="">
-    <option value=${eateryObject.id}>${eateryObject.businessName}</option>
-</select>`
-
-    return eateryHTMLRepresentation
-
-} 
-const goku =(object) => { console.log (object)
+const populateDropdown =(object) => { console.log (object)
   // Iterate the collection of attraction objects
 //   for (const currentEateryObject of eateryData) {
     // Convert the current location to its HTML representation
@@ -102,179 +78,3 @@ const goku =(object) => { console.log (object)
     eateryArticleElement.innerHTML = eateryHTML;
 //   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const eateryDetailsButton = document.querySelector(
-    ".show__eatery__details"
-  );
-  
-  eateryDetailsButton.addEventListener("click", (event) => {
-  
-      let eateryDropdown = document.querySelector(".eateries__dropdown").value;
-      console.log(eateryDropdown)
-  
-      let eateryDetailsData = [];
-  
-      const getEateriesDetails = () => {
-        return fetch(
-          "http://holidayroad.nss.team/eateries"
-        )
-          .then((httpResponse) => {
-            return httpResponse.json();
-          })
-          .then((arrayOfEateriesDetails) => {
-              eateryDetailsData = arrayOfEateriesDetails;
-  
-              let getEateryDetails = []
-              
-              for (i = 0; i < eateryDetailsData.length; i++) {
-                  // console.log(eateryDetailsData[i].id);
-                  if (eateryDropdown == eateryDetailsData[i].id) {
-                      // console.log(
-                      //   eateryDetailsData[i].id + eateryDetailsData[i].name + eateryDetailsData[i].description
-                       getEateryDetails = eateryDetailsData[i];
-                      // return eateryDetailsData[i]
-                      
-                  }
-              }
-              return getEateryDetails
-          }).then((data) => { 
-              eateryIteration(data)
-              
-              
-          })
-      };
-  
-      getEateriesDetails()
-  });
-  
-  /*
-Responsible for generating a list of eatery HTML
-representations, and putting in the browser
-*/
-/*const tacoEateryList = () => {
-    // Iterate the collection of eatery objects
-    for (const currentEateryObject of eateryData) {
-
-        // Convert the current location to its HTML representation
-        const eateryHTML = eateryConverter(currentEateryObject)
-
-        // Find the <article> element in index.html
-        const eateryArticleElement = document.querySelector(".eateries__dropdown")
-
-        // Put the location HTML representation inside the <article> element
-        eateryArticleElement.innerHTML += eateryHTML
-    }
-}
-*/
-
-/*
-    This function will convert a single location object to an
-    HTML representation and return it
-*/
-/*const specificEateryConverter = (eateryObject) => {
-
-    const eateryHTMLRepresentation = `
-    <option value="eatery">${eateryObject.businessName}</option>`
-
-    return eateryHTMLRepresentation
-
-} 
-   
-  const eateryIteration =(object) => { console.log (object)
-    // Iterate the collection of eatery objects
-  //   for (const currentEateryObject of eateryData) {
-      // Convert the current location to its HTML representation
-      let specificEatery = object
-      const specificHTML = specificEateryConverter(specificEatery);
-      
-  
-      // Find the <section>  (.eateryDetails)element in index.html
-      const eateryArticleElement = document.querySelector(".eatery__selected");
-  
-      // Put the location HTML representation inside the <article> element
-      eateryArticleElement.innerHTML = specificHTML;
-  //   }
-  };
-
-  const converter = (eateryObject) => { 
-    const eateryHTMLRepresentation = `
-                  <p>
-                      <li>id: ${eateryObject[0].businessName}</li>
-                      <li>name:${eateryObject.description}</li>
-                    </p>`;
-                
-  
-    return eateryHTMLRepresentation;
-  };
-  */
