9b318819b9881d1eff047b5c324dd5eed31684b5
Rob Mixon <mixonroberth@gmail.com>
2020-06-11 11:58:21 -0500

    getting ready to merge

--- scripts/parks/parkDetailButton.js
+++ scripts/parks/parkDetailButton.js
@@ -1,7 +1,7 @@
 
 
 let parkDetailsButton = document.querySelector(".show__park_details").addEventListener("click", clickEvent=> {
-    let parkDetailsArticle = document.querySelector(".park__details").classList.toggle("hidden")
+    let parkDetailsArticle = document.querySelector(".park__selected").classList.toggle("hidden")
     }
 )
 
