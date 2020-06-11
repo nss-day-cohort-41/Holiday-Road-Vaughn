5457fbb1330af729cca78091642117d876ef00ad
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-06-11 16:11:47 -0500

    started work on switching weather api

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -17,3 +17,14 @@ const weatherProvider = (zipCode) => {
 
 //need a then statement 
 //invoke weatherprovider inside then statment inside p
+
+/*
+new weather API url:
+https://api.openweathermap.org/data/2.5/onecall?lat=44.30777545&lon=-68.30063316&
+exclude=current&appid=${keys.weatherKey}
+
+old weather API url:
+https://api.openweathermap.org/data/2.5/forecast?q=${zipCode},us&units=imperial&appid=${keys.weatherKey}
+
+
+*/
